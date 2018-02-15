52bbf79d96de5b657316d2de6787e92846714562
yoli21 <y_venturchz@hotmail.com>
2018-02-15 10:39:26 -0500

    agregando funcionabilidad

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,32 @@
-$(document).ready(function () {
-    
+  // Setup an event listener to make an API call once auth is complete
   function onLinkedInLoad() {
-    IN.Event.on(IN, "auth", getProfileData);
-    console.log();
+    IN.Event.on(IN, "auth", shareContent);
   }
-  
+
+// Handle the successful return from the API call
+function onSuccess(data) {
+  console.log(data);
+}
+
+// Handle an error response from the API call
+function onError(error) {
+  console.log(error);
+}
+
+// Use the API call wrapper to share content on LinkedIn
+function shareContent() {
       
-    var err = error(function (data) {
-      console.log(data);
-    })
-   
-});
+  // Build the JSON payload containing the content to be shared
+  var payload = { 
+    "comment": "Check out developer.linkedin.com! http://linkd.in/1FC2PyG", 
+    "visibility": { 
+      "code": "anyone"
+    } 
+  };
+
+  IN.API.Raw("/people/~/shares?format=json")
+    .method("POST")
+    .body(JSON.stringify(payload))
+    .result(onSuccess)
+    .error(onError);
+}
