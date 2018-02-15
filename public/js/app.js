f591b8f7aa0034e6653ef13e5ed892ca11ae60b7
yoli21 <y_venturchz@hotmail.com>
2018-02-15 04:41:21 -0500

    api de linkedln

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
+<script type="text/javascript" src="//platform.linkedin.com/in.js">
+    api_key:   [API_KEY]
+    onLoad:    [ONLOAD]
+    authorize: [AUTHORIZE]
+    lang:      [LANG_LOCALE]
+</script>
+
 alert ('¡Sí funciono! :)');
